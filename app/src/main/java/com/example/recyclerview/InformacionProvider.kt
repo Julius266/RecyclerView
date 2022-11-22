0f293ff08be46679c979536e42eda926246b85f4
Julio Vinces <jvi3991zrj@gamil.com>
2022-11-21 19:17:47 -0500

    Initial commit

---
+++ app/src/main/java/com/example/recyclerview/InformacionProvider.kt
@@ -0,0 +1,25 @@
+package com.example.recyclerview
+
+class InformacionProvider {
+    companion object{
+        val info = listOf<Informacion>(
+            Informacion("Julio",17,"Software","https://raulperez.tieneblog.net/wp-content/uploads/2015/09/tux.jpg"),
+            Informacion("Juan",18,"Software","https://raulperez.tieneblog.net/wp-content/uploads/2015/09/tux.jpg"),
+            Informacion("Joao",19,"Software","https://raulperez.tieneblog.net/wp-content/uploads/2015/09/tux.jpg"),
+            Informacion("Jose",18,"Software","https://raulperez.tieneblog.net/wp-content/uploads/2015/09/tux.jpg"),
+            Informacion("Sebastian",19,"Software","https://raulperez.tieneblog.net/wp-content/uploads/2015/09/tux.jpg"),
+            Informacion("Johannes",18,"Software","https://raulperez.tieneblog.net/wp-content/uploads/2015/09/tux.jpg"),
+            Informacion("Byron",18,"Software","https://raulperez.tieneblog.net/wp-content/uploads/2015/09/tux.jpg"),
+            Informacion("Alejandro",18,"Software","https://raulperez.tieneblog.net/wp-content/uploads/2015/09/tux.jpg"),
+            Informacion("Luiggi",21,"Software","https://raulperez.tieneblog.net/wp-content/uploads/2015/09/tux.jpg"),
+            Informacion("Christian",19,"Software","https://raulperez.tieneblog.net/wp-content/uploads/2015/09/tux.jpg"),
+            Informacion("Aldair",19,"Software","https://raulperez.tieneblog.net/wp-content/uploads/2015/09/tux.jpg"),
+            Informacion("Eddie",18,"Software","https://raulperez.tieneblog.net/wp-content/uploads/2015/09/tux.jpg"),
+            Informacion("Luis",19,"Software","https://raulperez.tieneblog.net/wp-content/uploads/2015/09/tux.jpg"),
+            Informacion("Stiven",18,"Software","https://raulperez.tieneblog.net/wp-content/uploads/2015/09/tux.jpg"),
+            Informacion("Antonio",20,"Software","https://raulperez.tieneblog.net/wp-content/uploads/2015/09/tux.jpg"),
+            Informacion("Xavier",19,"Software","https://raulperez.tieneblog.net/wp-content/uploads/2015/09/tux.jpg"),
+            Informacion("Juan",18,"Software","https://raulperez.tieneblog.net/wp-content/uploads/2015/09/tux.jpg"))
+    }
+
+}
